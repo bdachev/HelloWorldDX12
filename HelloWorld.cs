105b9ee2cc2a04fff2951affac844e31e916e618
Boris Dachev <bdachev@gmail.com>
2015-05-28 22:09:06 +0300

    HelloWorld project compile shaders transform moved to descriptor table

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -45,6 +45,7 @@ namespace HelloWorld
         private CommandQueue commandQueue;
         private SwapChain swapChain;
         private DescriptorHeap descriptorHeapRT;
+        private DescriptorHeap descriptorHeapCB;
         private GraphicsCommandList commandList;
         private Resource renderTarget;
         private Rectangle scissorRectangle;
@@ -56,6 +57,7 @@ namespace HelloWorld
         private Resource vertexBuffer;
         private VertexBufferView[] vertexBufferView;
         private CpuDescriptorHandle[] descriptorsRT = new CpuDescriptorHandle[1];
+        private DescriptorHeap[] descriptorsHeaps = new DescriptorHeap[1];
         private Resource transform;
         private long currentFence;
         private int indexLastSwapBuf;
@@ -174,26 +176,56 @@ namespace HelloWorld
                 Type = DescriptorHeapType.RenderTargetView,
                 DescriptorCount = 1
             }));
+            descriptorHeapCB = Collect(device.CreateDescriptorHeap(new DescriptorHeapDescription()
+            {
+                Type = DescriptorHeapType.ConstantBufferViewShaderResourceViewUnorderedAccessView,
+                DescriptorCount = 2,
+                Flags = DescriptorHeapFlags.ShaderVisible,
+            }));
+            descriptorsHeaps[0] = descriptorHeapCB;
             var assembly = typeof(HelloWorld).Assembly;
+
+            var descrTable = new[]
+            {
+                new DescriptorRange
+                {
+                    BaseShaderRegister = 0,
+                    DescriptorCount = 1,
+                    RangeType = DescriptorRangeType.ConstantBufferView,
+                    RegisterSpace = 0,
+                    OffsetInDescriptorsFromTableStart = 1,
+                }
+            };
+            var descrTableBuf = Collect(DataBuffer.Create(descrTable));
 
             var rootParams = new[]
             {
+                //new RootParameter
+                //{
+                //    ParameterType = RootParameterType.ConstantBufferView,
+                //    ShaderVisibility = ShaderVisibility.Vertex,
+                //    Descriptor = new RootDescriptor
+                //    {
+                //        ShaderRegister = 0,
+                //        RegisterSpace = 0,
+                //    }
+                //},
                 new RootParameter
                 {
-                    ParameterType = RootParameterType.ConstantBufferView,
+                    ParameterType = RootParameterType.DescriptorTable,
                     ShaderVisibility = ShaderVisibility.Vertex,
-                    Descriptor = new RootDescriptor
+                    DescriptorTable = new RootDescriptorTable
                     {
-                        ShaderRegister = 0,
-                        RegisterSpace = 0,
+                        DescriptorRangeCount = 1,
+                        PDescriptorRanges = descrTableBuf.DataPointer,
                     }
-                },
+                }
             };
             var rsd = new RootSignatureDescription(RootSignatureFlags.AllowInputAssemblerInputLayout, rootParams);
             rootSignature = Collect(device.CreateRootSignature(rsd.Serialize()));
 
-            var vertexShaderByteCode = Utilities.ReadStream(assembly.GetManifestResourceStream("HelloWorld.Cube.vso"));
-            var pixelShaderByteCode = Utilities.ReadStream(assembly.GetManifestResourceStream("HelloWorld.Cube.pso"));
+            var vertexShaderByteCode = Utilities.ReadStream(assembly.GetManifestResourceStream("Shaders.Cube.vso"));
+            var pixelShaderByteCode = Utilities.ReadStream(assembly.GetManifestResourceStream("Shaders.Cube.pso"));
 
             var layout = new InputLayoutDescription(new InputElement[]
             {
@@ -201,6 +233,7 @@ namespace HelloWorld
                 new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 16, 0)
             });
 
+            #region pipeline state
             var psd = new GraphicsPipelineStateDescription
             {
                 InputLayout = layout,
@@ -218,10 +251,10 @@ namespace HelloWorld
             psd.RenderTargetFormats[0] = Format.R8G8B8A8_UNorm;
             psd.DepthStencilState.IsDepthEnabled = false;
             pipelineState = Collect(device.CreateGraphicsPipelineState(psd));
+            #endregion pipeline state
 
             #region vertices
             var vertices = new[]
-            #region vertex data
                                  {
                                       new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Front
                                       new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
@@ -265,9 +298,8 @@ namespace HelloWorld
                                       new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
                                       new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
                             };
-            #endregion vertex data
             #endregion vertices
-
+            #region vertex buffer
             // Instantiate Vertex buiffer from vertex data
             int sizeInBytes = vertices.Length * Utilities.SizeOf<Vector4>();
             vertexBuffer = Collect(device.CreateCommittedResource(
@@ -284,15 +316,23 @@ namespace HelloWorld
                     StrideInBytes = Utilities.SizeOf<Vector4>() * 2,
                 }
             };
-        var ptr = vertexBuffer.Map(0);
+            var ptr = vertexBuffer.Map(0);
             Utilities.Write(ptr, vertices, 0, vertices.Length);
             vertexBuffer.Unmap(0);
+            #endregion vertex buffer
 
+            #region transform
             transform = Collect(device.CreateCommittedResource(
                             new HeapProperties(HeapType.Upload),
                             HeapFlags.None,
                             new ResourceDescription(ResourceDimension.Buffer, 0, Utilities.SizeOf<Matrix>(), 1, 1, 1, Format.Unknown, 1, 0, TextureLayout.RowMajor, ResourceFlags.None),
                             ResourceStates.GenericRead));
+            device.CreateConstantBufferView(new ConstantBufferViewDescription
+            {
+                BufferLocation = transform.GPUVirtualAddress,
+                SizeInBytes = 256,
+            }, descriptorHeapCB.CPUDescriptorHandleForHeapStart);
+            #endregion transform
 
             // Create the main command list
             commandList = Collect(device.CreateCommandList(CommandListType.Direct, commandListAllocator, pipelineState));
@@ -346,10 +386,12 @@ namespace HelloWorld
             transform.Unmap(0);
             commandList.PipelineState = pipelineState;
             commandList.SetGraphicsRootSignature(rootSignature);
-            commandList.SetGraphicsRootConstantBufferView(0, transform.GPUVirtualAddress);
+            //commandList.SetGraphicsRootConstantBufferView(0, transform.GPUVirtualAddress);
+            commandList.SetDescriptorHeaps(1, descriptorsHeaps);
+            commandList.Descriptors.SetGraphicsRootDescriptorTable(0, descriptorHeapCB.GPUDescriptorHandleForHeapStart);
 
-	        // Use barrier to notify that we are using the RenderTarget to clear it
-	        commandList.ResourceBarrierTransition(renderTarget, ResourceStates.Present, ResourceStates.RenderTarget);
+            // Use barrier to notify that we are using the RenderTarget to clear it
+            commandList.ResourceBarrierTransition(renderTarget, ResourceStates.Present, ResourceStates.RenderTarget);
 
 	        // Clear the RenderTarget
 	        commandList.ClearRenderTargetView(descriptorHeapRT.CPUDescriptorHandleForHeapStart, new Color4((float)Math.Sin(time) * 0.25f + 0.5f, (float)Math.Sin(time * 0.5f) * 0.4f + 0.6f, 0.4f, 1.0f), 0, null);
